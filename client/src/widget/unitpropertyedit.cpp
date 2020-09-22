6738768d91d14cef2075f1fe14a149d39f6b5e2e
Reinder Feenstra <reinderfeenstra@gmail.com>
2020-09-22 23:38:47 +0200

    UnitPropertyEdit: setReadOnly if property not writable

--- client/src/widget/unitpropertyedit.cpp
+++ client/src/widget/unitpropertyedit.cpp
@@ -69,6 +69,7 @@ UnitPropertyEdit::UnitPropertyEdit(UnitProperty& property, QWidget *parent) :
   QHBoxLayout* l = new QHBoxLayout();
   l->setMargin(0);
 
+  m_valueLineEdit->setReadOnly(!m_property.isWritable());
   l->addWidget(m_valueLineEdit, 1);
   connect(m_valueLineEdit, &QLineEdit::textEdited, &m_property, &Property::setValueString);
 
