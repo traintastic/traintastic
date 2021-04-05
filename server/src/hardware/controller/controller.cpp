cba0a554a5b45428ba617c6d8f9726ad5765f802
Reinder Feenstra <reinderfeenstra@gmail.com>
2021-04-05 23:20:48 +0200

    controller: now responds to offline/online events

--- server/src/hardware/controller/controller.cpp
+++ server/src/hardware/controller/controller.cpp
@@ -3,7 +3,7 @@
  *
  * This file is part of the traintastic source code.
  *
- * Copyright (C) 2019-2020 Reinder Feenstra
+ * Copyright (C) 2019-2021 Reinder Feenstra
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -74,4 +74,24 @@ void Controller::worldEvent(WorldState state, WorldEvent event)
   const bool editable = contains(state, WorldState::Edit);
 
   commandStation.setAttributeEnabled(editable);
+
+  try
+  {
+    switch(event)
+    {
+      case WorldEvent::Offline:
+        active = false;
+        break;
+
+      case WorldEvent::Online:
+        active = true;
+        break;
+
+      default:
+        break;
+    }
+  }
+  catch(...)
+  {
+  }
 }
