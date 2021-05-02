dbc5f1ac3f5b5ebdc1c5b9f9d7504d97044230dc
Reinder Feenstra <reinderfeenstra@gmail.com>
2021-05-02 23:37:47 +0200

    missing in 9b9f1b1bbed3b89460abfee425c99e5f234dc5d3

---
+++ manual/builddoc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from argparse import ArgumentParser
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: " + sys.argv[0] + " <format>", file=sys.stderr)
+        sys.exit(1)
+
+    args = sys.argv[2:]  # remove program name and format
+
+    # Standard options:
+    parser = ArgumentParser()
+    parser.add_argument('--base-dir', default=os.path.join(os.path.dirname(__file__), 'traintasticmanual'))
+    parser.add_argument('--output-dir')
+    parser.add_argument('--language', default='en-us')
+    parser.add_argument('--version', default=None)
+
+    if sys.argv[1] == 'html-single-page':
+        from traintasticmanualbuilder.htmlsinglepage import HTMLSinglePageBuilder
+        HTMLSinglePageBuilder(parser.parse_args(args)).build()
+    else:
+        print("Unknown format: " + sys.argv[1], file=sys.stderr)
+        sys.exit(1)
