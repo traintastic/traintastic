aaf8831bcd3a2fbeb3e5a0000b7ccb826b866c46
Reinder Feenstra <reinderfeenstra@gmail.com>
2021-05-24 00:26:20 +0200

    input: set name to id when created

--- server/src/hardware/input/input.cpp
+++ server/src/hardware/input/input.cpp
@@ -27,7 +27,7 @@
 
 Input::Input(const std::weak_ptr<World> world, std::string_view _id) :
   IdObject(world, _id),
-  name{this, "name", "", PropertyFlags::ReadWrite | PropertyFlags::Store},
+  name{this, "name", id, PropertyFlags::ReadWrite | PropertyFlags::Store},
   value{this, "value", TriState::Undefined, PropertyFlags::ReadOnly | PropertyFlags::StoreState}
 {
   auto w = world.lock();
