aeb1754619844c69a573e3dd2d2347e3515ae0d2
Reinder Feenstra <reinderfeenstra@gmail.com>
2021-06-06 23:20:32 +0200

    board name now equal to id when created

--- server/src/board/board.cpp
+++ server/src/board/board.cpp
@@ -29,7 +29,7 @@
 
 Board::Board(const std::weak_ptr<World>& world, std::string_view _id) :
   IdObject(world, _id),
-  name{this, "name", "", PropertyFlags::ReadWrite | PropertyFlags::Store},
+  name{this, "name", id, PropertyFlags::ReadWrite | PropertyFlags::Store},
   left{this, "left", 0, PropertyFlags::ReadOnly | PropertyFlags::Store},
   top{this, "top", 0, PropertyFlags::ReadOnly | PropertyFlags::Store},
   right{this, "right", 0, PropertyFlags::ReadOnly | PropertyFlags::Store},
