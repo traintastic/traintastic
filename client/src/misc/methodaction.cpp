e6ccd75230a582a66af03af16d3789dd83348fa8
Reinder Feenstra <reinderfeenstra@gmail.com>
2021-06-25 00:29:07 +0200

    Added MethodAction, a QAction based wrapper for methods

---
+++ client/src/misc/methodaction.cpp
@@ -0,0 +1,72 @@
+/**
+ * client/src/misc/methodaction.cpp
+ *
+ * This file is part of the traintastic source code.
+ *
+ * Copyright (C) 2021 Reinder Feenstra
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "methodaction.hpp"
+#include "../network/method.hpp"
+
+MethodAction::MethodAction(Method& method, QObject* parent) :
+  QAction(parent),
+  m_method{method}
+{
+  init();
+}
+
+MethodAction::MethodAction(const QIcon &icon, Method& method, QObject* parent) :
+  QAction(icon, QString(), parent),
+  m_method{method}
+{
+  init();
+}
+
+MethodAction::MethodAction(const QIcon &icon, Method& method, std::function<void()> triggered, QObject* parent) :
+  QAction(icon, QString(), parent),
+  m_method{method}
+{
+  init(false);
+  connect(this, &QAction::triggered, std::move(triggered));
+}
+
+void MethodAction::init(bool connectTriggeredSignalToMethodIfCompatible)
+{
+  setText(m_method.displayName());
+  setEnabled(m_method.getAttributeBool(AttributeName::Enabled, true));
+  setVisible(m_method.getAttributeBool(AttributeName::Visible, true));
+
+  connect(&m_method, &Method::attributeChanged, this,
+    [this](AttributeName name, const QVariant& value)
+    {
+      if(name == AttributeName::Enabled)
+        setEnabled(value.toBool());
+      else if(name == AttributeName::Visible)
+        setVisible(value.toBool());
+      else if(name == AttributeName::DisplayName)
+        setText(m_method.displayName());
+    });
+
+  // if no return value and no arguments -> setup execute
+  if(connectTriggeredSignalToMethodIfCompatible && m_method.resultType() == ValueType::Invalid && m_method.argumentTypes().isEmpty())
+    connect(this, &QAction::triggered, this,
+      [this]()
+      {
+        m_method.call();
+      });
+}
---
+++ client/src/misc/methodaction.hpp
@@ -0,0 +1,52 @@
+/**
+ * client/src/misc/methodaction.hpp
+ *
+ * This file is part of the traintastic source code.
+ *
+ * Copyright (C) 2021 Reinder Feenstra
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TRAINTASTIC_CLIENT_MISC_METHODACTION_HPP
+#define TRAINTASTIC_CLIENT_MISC_METHODACTION_HPP
+
+#include <QAction>
+
+class Method;
+
+/**
+ * @brief Action wrapper for method's
+ *
+ * The action's text is set to the method display name.
+ * The method's displayName, enabled and visible attributes are monitored for changes and applied to the action.
+ *
+ * If the method has no return value and no arguments it's will call the method when triggered.
+ * For other method type this has to be done externally.
+ */
+class MethodAction : public QAction
+{
+  private:
+    Method& m_method;
+
+    void init(bool connectTriggeredSignalToMethodIfCompatible = true);
+
+  public:
+    MethodAction(Method& method, QObject* parent = nullptr);
+    MethodAction(const QIcon &icon, Method& method, QObject* parent = nullptr);
+    MethodAction(const QIcon &icon, Method& method, std::function<void()> triggered, QObject* parent = nullptr);
+};
+
+#endif
