1928ea1be2ba48f8a0dea7ecca7a916eae27bb92
Reinder Feenstra <reinderfeenstra@gmail.com>
2022-06-07 00:12:07 +0200

    CI: let CMake detect GitHub action run, try 6

--- shared/traintastic.cmake
+++ shared/traintastic.cmake
@@ -1,27 +1,35 @@
+# Version
+set(TRAINTASTIC_VERSION 0.0.2)
 
+# Platform
+if(UNIX AND NOT APPLE AND NOT MINGW)
+  set(LINUX TRUE)
+endif()
+
+# CI/CD
 if(DEFINED ENV{GITHUB_ACTIONS})
   message(STATUS "We're on GitHub Actions")
-
-  message(STATUS "ENV{GITHUB_REF_TYPE} = $ENV{GITHUB_REF_TYPE}")
-  message(STATUS "ENV{CI_REF_NAME_SLUG} = $ENV{CI_REF_NAME_SLUG}")
-  message(STATUS "ENV{GITHUB_RUN_NUMBER} = $ENV{GITHUB_RUN_NUMBER}")
-  message(STATUS "ENV{CI_SHA_SHORT} = $ENV{CI_SHA_SHORT}")
 
   if(ENV{GITHUB_REF_TYPE} EQUAL "branch")
 
     set(TRAINTASTIC_CODENAME "$ENV{CI_REF_NAME_SLUG}-$ENV{GITHUB_RUN_NUMBER}-$ENV{CI_SHA_SHORT}")
 
-    if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+    if(LINUX)
       execute_process(COMMAND lsb_release -i -s OUTPUT_VARIABLE LSB_RELEASE_ID)
       execute_process(COMMAND lsb_release -c -s OUTPUT_VARIABLE LSB_RELEASE_CODENAME)
 
       string(TOLOWER ${LSB_RELEASE_ID} LSB_RELEASE_ID)
       string(TOLOWER ${LSB_RELEASE_CODENAME} LSB_RELEASE_CODENAME)
 
-      message(STATUS "lsb_release id: ${LSB_RELEASE_ID}")
-      message(STATUS "lsb_release codename: ${LSB_RELEASE_CODENAME}")
+      message(STATUS "lsb_release id is ${LSB_RELEASE_ID}")
+      message(STATUS "lsb_release codename is ${LSB_RELEASE_CODENAME}")
 
       set(DEBIAN_PACKAGE_VERSION_EXTRA "~${LSB_RELEASE_ID}~${LSB_RELEASE_CODENAME}~$ENV{CI_REF_NAME_SLUG}~$ENV{GITHUB_RUN_NUMBER}~$ENV{CI_SHA_SHORT}")
     endif()
   endif()
+endif()
+
+# Codename
+if(TRAINTASTIC_CODENAME)
+  add_definitions(-DTRAINTASTIC_CODENAME="${TRAINTASTIC_CODENAME}")
 endif()
