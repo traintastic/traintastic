f0989ff85c243bed3bd786e39c52c1625776cd16
Reinder Feenstra <reinderfeenstra@gmail.com>
2022-09-02 19:41:40 +0200

    fix: added missing include for ubuntu 22.04

--- server/src/lua/sandbox.hpp
+++ server/src/lua/sandbox.hpp
@@ -3,7 +3,7 @@
  *
  * This file is part of the traintastic source code.
  *
- * Copyright (C) 2019-2020 Reinder Feenstra
+ * Copyright (C) 2019-2020,2022 Reinder Feenstra
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -26,6 +26,7 @@
 #include <memory>
 #include <map>
 #include <algorithm>
+#include <limits>
 #include <cassert>
 #include <lua.hpp>
 
