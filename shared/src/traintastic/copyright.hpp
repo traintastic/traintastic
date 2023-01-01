48d2b2935c67a281c5cac6497c0eb672963d3319
Reinder Feenstra <reinderfeenstra@gmail.com>
2023-01-01 01:04:23 +0100

    Happy new year! 🎆 Bumped copyright year to 2023

--- shared/src/traintastic/copyright.hpp
+++ shared/src/traintastic/copyright.hpp
@@ -3,7 +3,7 @@
  *
  * This file is part of the traintastic source code.
  *
- * Copyright (C) 2022 Reinder Feenstra
+ * Copyright (C) 2022-2023 Reinder Feenstra
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,6 +23,6 @@
 #ifndef TRAINTASTIC_SHARED_TRAINTASTIC_COPYRIGHT_HPP
 #define TRAINTASTIC_SHARED_TRAINTASTIC_COPYRIGHT_HPP
 
-#define TRAINTASTIC_COPYRIGHT "Copyright (c) 2019-2022 Reinder Feenstra"
+#define TRAINTASTIC_COPYRIGHT "Copyright (c) 2019-2023 Reinder Feenstra"
 
 #endif
