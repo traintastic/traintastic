d471111947755d94daaa0ca9cc349cf410ff62d8
Filippo Gentile <gentilefilippo01@gmail.com>
2023-03-27 22:25:30 +0200

    Train: new active property
    It is mirrored in RailVehicle activeTrain property.
    
    Activating a Train is an atomic operation:
    - Every contained vehicle must not be in other active train
    - It also must be stopped
    
    Train: setTrainActive() new function to activate Train

--- server/src/train/train.cpp
+++ server/src/train/train.cpp
@@ -115,6 +115,8 @@ Train::Train(World& world, std::string_view _id) :
     }},
   vehicles{this, "vehicles", nullptr, PropertyFlags::ReadOnly | PropertyFlags::Store | PropertyFlags::SubObject},
   powered{this, "powered", false, PropertyFlags::ReadOnly | PropertyFlags::NoStore | PropertyFlags::ScriptReadOnly},
+  active{this, "active", false, PropertyFlags::ReadWrite | PropertyFlags::NoStore | PropertyFlags::ScriptReadOnly,
+           [](bool){}, std::bind(&Train::setTrainActive, this, std::placeholders::_1)},
   notes{this, "notes", "", PropertyFlags::ReadWrite | PropertyFlags::Store}
 {
   vehicles.setValueInternal(std::make_shared<TrainVehicleList>(*this, vehicles.name()));
@@ -154,6 +156,10 @@ Train::Train(World& world, std::string_view _id) :
   m_interfaceItems.add(weight);
   m_interfaceItems.add(overrideWeight);
   m_interfaceItems.add(vehicles);
+
+  Attributes::addEnabled(active, true);
+  m_interfaceItems.add(active);
+
   Attributes::addObjectEditor(powered, false);
   m_interfaceItems.add(powered);
   Attributes::addDisplayName(notes, DisplayName::Object::notes);
@@ -326,4 +332,48 @@ void Train::updateEnabled()
   Attributes::setEnabled(vehicles->remove, stopped);
   Attributes::setEnabled(vehicles->move, stopped);
   Attributes::setEnabled(vehicles->reverse, stopped);
+}
+
+bool Train::setTrainActive(bool val)
+{
+  auto self = this->shared_ptr<Train>();
+
+  if(val)
+  {
+    //To activate a train, ensure all vehicles are stopped and free
+    for(const auto& vehicle : *vehicles)
+    {
+      assert(vehicle->activeTrain.value() != self);
+      if(vehicle->activeTrain.value())
+      {
+        return false; //Not free
+      }
+
+      if(auto decoder = vehicle->decoder.value(); decoder && !almostZero(decoder->throttle.value()))
+      {
+        return false; //Already running
+      }
+    }
+
+    //Now really activate
+    for(const auto& vehicle : *vehicles)
+    {
+      vehicle->activeTrain.setValueInternal(self);
+    }
+  }
+  else
+  {
+    //To deactivate a Train it must be stopped first
+    if(!isStopped)
+      return false;
+
+    //Deactivate all vehicles
+    for(const auto& vehicle : *vehicles)
+    {
+      assert(vehicle->activeTrain.value() == self);
+      vehicle->activeTrain.setValueInternal(nullptr);
+    }
+  }
+
+  return true;
 }
