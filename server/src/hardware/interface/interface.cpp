206f8a828882e92e1953fde7086873cbaf41f36d
Reinder Feenstra <reinderfeenstra@gmail.com>
2023-04-13 23:37:20 +0200

    interface: make sure interface is offline before destroying it see #52

--- server/src/hardware/interface/interface.cpp
+++ server/src/hardware/interface/interface.cpp
@@ -3,7 +3,7 @@
  *
  * This file is part of the traintastic source code.
  *
- * Copyright (C) 2021-2022 Reinder Feenstra
+ * Copyright (C) 2021-2023 Reinder Feenstra
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -65,6 +65,7 @@ void Interface::addToWorld()
 
 void Interface::destroying()
 {
+  online = false; // make sure interface is offline before destroying it
   m_world.interfaces->removeObject(shared_ptr<Interface>());
   IdObject::destroying();
 }
