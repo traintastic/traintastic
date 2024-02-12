e69e174868a298ff303824ba20c3d8bda5bfbeeb
Reinder Feenstra <reinderfeenstra@gmail.com>
2024-02-12 17:01:59 +0100

    fix: ignore stringop-overflow for GCC too

--- server/src/hardware/protocol/z21/messages.cpp
+++ server/src/hardware/protocol/z21/messages.cpp
@@ -302,13 +302,13 @@ void LanX::updateChecksum(uint8_t len)
 {
   uint8_t val = XpressNet::calcChecksum(*reinterpret_cast<const XpressNet::Message*>(&xheader), len);
   uint8_t* checksum = &xheader + len + 1;
-#ifdef __MINGW32__
+#if defined(__GNUC__) || defined(__MINGW32__)
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wstringop-overflow"
 #endif
   *checksum = val;
-#ifdef __MINGW32__
-  #pragma GCC diagnostic pop  
+#if defined(__GNUC__) || defined(__MINGW32__)
+  #pragma GCC diagnostic pop
 #endif
 }
 
