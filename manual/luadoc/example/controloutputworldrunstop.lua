4da086446dfd05d5a972100b23709b807eedf213
Reinder Feenstra <reinderfeenstra@gmail.com>
2024-03-14 23:27:48 +0100

    updated lua example for output refactor, see #95

--- manual/luadoc/example/controloutputworldrunstop.lua
+++ manual/luadoc/example/controloutputworldrunstop.lua
@@ -1,20 +1,15 @@
+-- get the output to control
+output = world.get_object('loconet_1').get_output(enum.output_channel.ACCESSORY, 1)
+
 -- world event handler
 function world_event(state)
-  -- get the output to control
-  local output = world.get_object('output_1')
-
   -- check if the RUN flag is set in the world state
   if world.state.contains(set.world_state.RUN) then
-    -- turn output off
-    output.set_value(false)
+    output.set_value(enum.output_pair_value.SECOND)
   else
-    -- turn output on
-    output.set_value(true)
+    output.set_value(enum.output_pair_value.FIRST)
   end
 end
 
 -- register the event handler
 world.on_event(world_event)
-
--- call event handler once at script start to set output to current world state
-world_event(world.state)
