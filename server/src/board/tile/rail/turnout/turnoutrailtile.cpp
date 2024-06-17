63f22f0cf5b7c78d43cb8e64e533bf1dab8cb00b
Filippo Gentile <filippogentile@disroot.org>
2024-06-17 14:41:06 +0200

    server: Add log warning when output is externally changed while locked

--- server/src/board/tile/rail/turnout/turnoutrailtile.cpp
+++ server/src/board/tile/rail/turnout/turnoutrailtile.cpp
@@ -30,6 +30,7 @@
 #include "../blockrailtile.hpp"
 #include "../../../../train/trainblockstatus.hpp"
 #include "../../../../train/train.hpp"
+#include "../../../../log/log.hpp"
 
 TurnoutRailTile::TurnoutRailTile(World& world, std::string_view _id, TileId tileId, size_t connectors) :
   RailTile(world, _id, tileId),
@@ -170,6 +171,8 @@ void TurnoutRailTile::connectOutputMap()
           }
           else
           {
+            Log::log(id, LogMessage::W3003_LOCKED_OUTPUT_CHANGED);
+
             // We cannot lock this turnout. Stop all trains in this path
             if(auto blockPath = m_reservedPath.lock())
             {
