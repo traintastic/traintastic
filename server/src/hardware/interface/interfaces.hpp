16c43ae171512e03c8baaed33bc4a19edf138d22
Reinder Feenstra <reinderfeenstra@gmail.com>
2024-10-21 23:51:01 +0200

    converted classList to function, now includes can be in cpp

--- server/src/hardware/interface/interfaces.hpp
+++ server/src/hardware/interface/interfaces.hpp
@@ -3,7 +3,7 @@
  *
  * This file is part of the traintastic source code.
  *
- * Copyright (C) 2021-2023 Reinder Feenstra
+ * Copyright (C) 2021-2024 Reinder Feenstra
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,36 +24,12 @@
 #define TRAINTASTIC_SERVER_HARDWARE_INTERFACE_INTERFACES_HPP
 
 #include "interface.hpp"
-#include "../../utils/makearray.hpp"
-
-#include "dccexinterface.hpp"
-#include "ecosinterface.hpp"
-#include "hsi88.hpp"
-#include "loconetinterface.hpp"
-#include "marklincaninterface.hpp"
-#include "traintasticdiyinterface.hpp"
-#include "withrottleinterface.hpp"
-#include "wlanmausinterface.hpp"
-#include "xpressnetinterface.hpp"
-#include "z21interface.hpp"
 
 struct Interfaces
 {
   static constexpr std::string_view classIdPrefix = "interface.";
 
-  static constexpr auto classList = makeArray(
-    DCCEXInterface::classId,
-    ECoSInterface::classId,
-    HSI88Interface::classId,
-    LocoNetInterface::classId,
-    MarklinCANInterface::classId,
-    TraintasticDIYInterface::classId,
-    WiThrottleInterface::classId,
-    WlanMausInterface::classId,
-    XpressNetInterface::classId,
-    Z21Interface::classId
-  );
-
+  static tcb::span<const std::string_view> classList();
   static std::shared_ptr<Interface> create(World& world, std::string_view classId, std::string_view id = std::string_view{});
 };
 
