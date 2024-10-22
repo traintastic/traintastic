d996d631f0843a93be2326bee05b5eb9ff385aa1
Reinder Feenstra <reinderfeenstra@gmail.com>
2024-10-22 23:42:32 +0200

    fixes for catch2 v2 -> v3 upgrade

--- server/test/lua/script.cpp
+++ server/test/lua/script.cpp
@@ -20,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include "../../src/world/world.hpp"
 #include "../../src/core/method.tpp"
 #include "../../src/core/objectproperty.tpp"
