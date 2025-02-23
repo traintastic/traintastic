11bc22a2800690f8eb3a80db11e8274d9e432d52
Reinder Feenstra <reinderfeenstra@gmail.com>
2025-02-23 22:37:15 +0100

    [server] added IOHandler for communicating with Traintastic simulator

---
+++ server/src/simulator/simulatoriohandler.cpp
@@ -0,0 +1,202 @@
+/**
+ * This file is part of Traintastic,
+ * see <https://github.com/traintastic/traintastic>.
+ *
+ * Copyright (C) 2025 Reinder Feenstra
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "simulatoriohandler.hpp"
+#include <traintastic/simulator/protocol.hpp>
+
+SimulatorIOHandler::SimulatorIOHandler(boost::asio::io_context& ioContext, std::string hostname, uint16_t port)
+  : m_hostname{std::move(hostname)}
+  , m_port{port}
+  , m_ioContext{ioContext}
+  , m_socket{ioContext}
+{
+}
+
+void SimulatorIOHandler::start()
+{
+  boost::system::error_code ec;
+
+  m_endpoint.port(m_port);
+  m_endpoint.address(boost::asio::ip::make_address(m_hostname, ec));
+  if(ec)
+  {
+    //throw LogMessageException(LogMessage::E2003_MAKE_ADDRESS_FAILED_X, ec);
+  }
+
+  m_socket.async_connect(m_endpoint,
+    [this](const boost::system::error_code& err)
+    {
+      if(!err)
+      {
+        m_socket.set_option(boost::asio::socket_base::linger(true, 0));
+        m_socket.set_option(boost::asio::ip::tcp::no_delay(true));
+
+        m_connected = true;
+
+        read();
+        write();
+      }
+      else if(err != boost::asio::error::operation_aborted)
+      {
+        //EventLoop::call(
+        //  [this, err]()
+        //  {
+            //Log::log(m_kernel.logId, LogMessage::E2005_SOCKET_CONNECT_FAILED_X, err);
+            //error();
+        //  });
+      }
+    });
+}
+
+void SimulatorIOHandler::stop()
+{
+}
+
+void SimulatorIOHandler::sendLocomotiveSpeedDirection(DecoderProtocol protocol, uint16_t address, uint8_t speed, Direction direction, bool emergencyStop)
+{
+  const SimulatorProtocol::LocomotiveSpeedDirection message(address, protocol, speed, direction, emergencyStop);
+  send(message);
+}
+
+bool SimulatorIOHandler::send(const SimulatorProtocol::Message& message)
+{
+  if(m_writeBufferOffset + message.size > m_writeBuffer.size())
+  {
+    return false;
+  }
+
+  const bool wasEmpty = m_writeBufferOffset == 0;
+  memcpy(m_writeBuffer.data() + m_writeBufferOffset, &message, message.size);
+  m_writeBufferOffset += message.size;
+
+  if(wasEmpty && m_connected)
+  {
+    write();
+  }
+
+  return true;
+}
+
+void SimulatorIOHandler::receive(const SimulatorProtocol::Message& message)
+{
+  using namespace SimulatorProtocol;
+
+  switch(message.opCode)
+  {
+    case OpCode::Power:
+      break;
+
+    case OpCode::LocomotiveSpeedDirection:
+      if(onLocomotiveSpeedDirection)
+      {
+        const auto& m = static_cast<const LocomotiveSpeedDirection&>(message);
+        onLocomotiveSpeedDirection(m.protocol, m.address, m.speed, m.direction, m.emergencyStop != 0);
+      }
+      break;
+
+    case OpCode::SensorChanged:
+      if(onSensorChanged)
+      {
+        const auto& m = static_cast<const SensorChanged&>(message);
+        onSensorChanged(m.channel, m.address, m.value);
+      }
+      break;
+  }
+}
+
+void SimulatorIOHandler::read()
+{
+  m_socket.async_read_some(boost::asio::buffer(m_readBuffer.data() + m_readBufferOffset, m_readBuffer.size() - m_readBufferOffset),
+    [this](const boost::system::error_code& ec, std::size_t bytesTransferred)
+    {
+      if(!ec)
+      {
+        const std::byte* pos = m_readBuffer.data();
+        bytesTransferred += m_readBufferOffset;
+
+        while(bytesTransferred > 1)
+        {
+          const auto* message = reinterpret_cast<const SimulatorProtocol::Message*>(pos);
+
+          if(bytesTransferred < message->size)
+          {
+            break;
+          }
+
+          receive(*message);
+          pos += message->size;
+          bytesTransferred -= message->size;
+        }
+
+        if(bytesTransferred != 0)
+        {
+          memmove(m_readBuffer.data(), pos, bytesTransferred);
+        }
+        m_readBufferOffset = bytesTransferred;
+
+        read();
+      }
+      else
+      {
+        //EventLoop::call(
+        //  [this, ec]()
+        //  {
+            //Log::log(m_kernel.logId, LogMessage::E2008_SOCKET_READ_FAILED_X, ec);
+            //error();
+        //  });
+      }
+    });
+}
+
+void SimulatorIOHandler::write()
+{
+  if(m_writeBufferOffset == 0) /*[[unlikely]]*/
+  {
+    return;
+  }
+
+  m_socket.async_write_some(boost::asio::buffer(m_writeBuffer.data(), m_writeBufferOffset),
+    [this](const boost::system::error_code& ec, std::size_t bytesTransferred)
+    {
+      if(!ec)
+      {
+        if(bytesTransferred < m_writeBufferOffset)
+        {
+          m_writeBufferOffset -= bytesTransferred;
+          memmove(m_writeBuffer.data(), m_writeBuffer.data() + bytesTransferred, m_writeBufferOffset);
+          write();
+        }
+        else
+        {
+          m_writeBufferOffset = 0;
+        }
+      }
+      else if(ec != boost::asio::error::operation_aborted)
+      {
+        //EventLoop::call(
+        //  [this, ec]()
+        //  {
+            //Log::log(m_kernel.logId, LogMessage::E2007_SOCKET_WRITE_FAILED_X, ec);
+            //error();
+        //  });
+      }
+    });
+}
---
+++ server/src/simulator/simulatoriohandler.hpp
@@ -0,0 +1,68 @@
+/**
+ * This file is part of Traintastic,
+ * see <https://github.com/traintastic/traintastic>.
+ *
+ * Copyright (C) 2025 Reinder Feenstra
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TRAINTASTIC_SERVER_SIMULATOR_SIMULATORIOHANDLER_HPP
+#define TRAINTASTIC_SERVER_SIMULATOR_SIMULATORIOHANDLER_HPP
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+namespace SimulatorProtocol
+{
+  struct Message;
+}
+
+enum class DecoderProtocol : uint8_t;
+enum class Direction : uint8_t;
+
+class SimulatorIOHandler
+{
+  private:
+    const std::string m_hostname;
+    const uint16_t m_port;
+    boost::asio::io_context& m_ioContext;
+    boost::asio::ip::tcp::socket m_socket;
+    boost::asio::ip::tcp::endpoint m_endpoint;
+    bool m_connected = false;
+    std::array<std::byte, 1024> m_readBuffer;
+    size_t m_readBufferOffset = 0;
+    std::array<std::byte, 1024> m_writeBuffer;
+    size_t m_writeBufferOffset = 0;
+
+    bool send(const SimulatorProtocol::Message& message);
+    void receive(const SimulatorProtocol::Message& message);
+
+    void read();
+    void write();
+
+  public:
+    SimulatorIOHandler(boost::asio::io_context& ioContext, std::string hostname, uint16_t port);
+
+    void start();
+    void stop();
+
+    void sendLocomotiveSpeedDirection(DecoderProtocol protocol, uint16_t address, uint8_t speed, Direction direction, bool emergencyStop);
+
+    std::function<void(DecoderProtocol, uint16_t, uint8_t, Direction, bool)> onLocomotiveSpeedDirection;
+    std::function<void(uint16_t, uint16_t, bool)> onSensorChanged;
+};
+
+#endif
