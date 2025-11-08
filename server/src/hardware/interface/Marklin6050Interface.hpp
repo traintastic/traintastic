46332a932413f5c65fcec6b575d7e960dba19b21
kamil00110 <68923965+kamil00110@users.noreply.github.com>
2025-11-08 19:10:53 +0100

    Implement dummy Märklin 6050 interface
    Added a dummy interface for the Märklin 6050 hardware to support Traintastic.

--- server/src/hardware/interface/Marklin6050Interface.hpp
+++ server/src/hardware/interface/Marklin6050Interface.hpp
@@ -1,1 +1,39 @@
+/**
+ * server/src/hardware/interface/Marklin6050Interface.hpp
+ *
+ * Dummy Märklin 6050 interface for Traintastic
+ * Copyright (C) 2025
+ */
+
+#ifndef TRAINTASTIC_SERVER_HARDWARE_INTERFACE_MARKLIN6050INTERFACE_HPP
+#define TRAINTASTIC_SERVER_HARDWARE_INTERFACE_MARKLIN6050INTERFACE_HPP
+
+#include "interface.hpp"
+#include "../../core/objectproperty.hpp"
+
+/**
+ * \brief Dummy interface for the Märklin 6050 hardware
+ */
+class Marklin6050Interface final : public Interface
+{
+  CLASS_ID("interface.marklin6050")
+  DEFAULT_ID("marklin6050")
+  CREATE_DEF(Marklin6050Interface)
+
+private:
+  void updateEnabled();
+
+protected:
+  void addToWorld() final;
+  void loaded() final;
+  void destroying() final;
+  void worldEvent(WorldState state, WorldEvent event) final;
+  void onlineChanged(bool value) final;
+  bool setOnline(bool& value, bool simulation) final;
+
+public:
+  Marklin6050Interface(World& world, std::string_view id);
+};
+
+#endif
 
