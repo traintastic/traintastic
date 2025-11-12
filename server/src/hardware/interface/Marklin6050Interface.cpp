750a1f7da32dfe0cd34cd6a373c8f63183c64d4e
kamil00110 <68923965+kamil00110@users.noreply.github.com>
2025-11-12 01:55:05 +0100

    Update Marklin6050Interface.cpp

--- server/src/hardware/interface/Marklin6050Interface.cpp
+++ server/src/hardware/interface/Marklin6050Interface.cpp
@@ -10,101 +10,93 @@
 #include "../../world/world.hpp"
 #include "../../utils/displayname.hpp"
 
-#include <vector>
-#include <string>
-#include <algorithm>
-
 CREATE_IMPL(Marklin6050Interface)
 
-// Platform-independent serial enumeration
-namespace Serial {
-    inline std::vector<std::string> getPortList() {
-        std::vector<std::string> ports;
-
-#if defined(_WIN32)
-        for (int i = 1; i <= 256; ++i)
-            ports.push_back("COM" + std::to_string(i));
-#elif defined(__APPLE__)
-        ports.push_back("/dev/tty.usbserial");
-        ports.push_back("/dev/tty.usbmodem");
-#else
-        ports.push_back("/dev/ttyS0");
-        ports.push_back("/dev/ttyS1");
-        ports.push_back("/dev/ttyUSB0");
-        ports.push_back("/dev/ttyUSB1");
-#endif
-        return ports;
-    }
-
-    inline bool isValidPort(const std::string& port) {
-        auto ports = getPortList();
-        return std::find(ports.begin(), ports.end(), port) != ports.end();
-    }
-
-    inline bool testOpen(const std::string& port) {
-        (void)port;
-        return true;
-    }
-}
-
 Marklin6050Interface::Marklin6050Interface(World& world, std::string_view idValue)
-    : Interface(world, idValue),
-      serialPort(this, "serialPort", "", PropertyFlags{},   // <- fixed
-                 [this](const std::string& value) {
-                     serialPortChanged(value);
-                 })
+  : Interface(world, idValue),
+    serialPort(this, "serialPort", "", PropertyFlags::None)
 {
-    name = "Märklin 6050";
+  name = "Märklin 6050";
 
-    // Populate serial port choices
-    auto availablePorts = Serial::getPortList();
-    if (!availablePorts.empty())
-        serialPort = availablePorts.front();
+  // Optionally: populate default ports at load time
+  auto availablePorts = Marklin6050::Serial::getPortList();
+  if (!availablePorts.empty())
+    serialPort = availablePorts.front();
 }
 
-void Marklin6050Interface::addToWorld() {
-    Interface::addToWorld();
+void Marklin6050Interface::addToWorld()
+{
+  Interface::addToWorld();
 }
 
-void Marklin6050Interface::loaded() {
-    Interface::loaded();
-    updateEnabled();
+void Marklin6050Interface::loaded()
+{
+  Interface::loaded();
+  updateEnabled();
 }
 
-void Marklin6050Interface::destroying() {
-    Interface::destroying();
+void Marklin6050Interface::destroying()
+{
+  Interface::destroying();
 }
 
-void Marklin6050Interface::worldEvent(WorldState state, WorldEvent event) {
-    Interface::worldEvent(state, event);
-    switch (event) {
-        case WorldEvent::PowerOn:
-        case WorldEvent::PowerOff:
-        default:
-            break;
+void Marklin6050Interface::worldEvent(WorldState state, WorldEvent event)
+{
+  Interface::worldEvent(state, event);
+
+  switch (event)
+  {
+    case WorldEvent::PowerOn:
+      // If desired, automatically go online here
+      break;
+
+    case WorldEvent::PowerOff:
+      break;
+
+    default:
+      break;
+  }
+}
+
+bool Marklin6050Interface::setOnline(bool& value, bool /*simulation*/)
+{
+  if (value)
+  {
+    // Attempt to open selected serial port
+    std::string port = serialPort.value();
+    if (!Marklin6050::Serial::isValidPort(port))
+    {
+      value = false;
+      setState(InterfaceState::Offline);
+      return false;
     }
-}
 
-bool Marklin6050Interface::setOnline(bool& value, bool /*simulation*/) {
-    if (value) {
-        std::string port = serialPort.value();
-        if (!Serial::isValidPort(port) || !Serial::testOpen(port)) {
-            value = false;
-            setState(InterfaceState::Offline);
-            return false;
-        }
-        setState(InterfaceState::Online);
-    } else {
-        setState(InterfaceState::Offline);
+    // Optionally test open
+    if (!Marklin6050::Serial::testOpen(port))
+    {
+      value = false;
+      setState(InterfaceState::Offline);
+      return false;
     }
-    updateEnabled();
-    return true;
+
+    setState(InterfaceState::Online);
+  }
+  else
+  {
+    setState(InterfaceState::Offline);
+  }
+
+  updateEnabled();
+  return true;
 }
 
-void Marklin6050Interface::updateEnabled() {
-    // Add any UI or logic updates
+void Marklin6050Interface::updateEnabled()
+{
+  // You can add conditional logic if needed
 }
 
-void Marklin6050Interface::serialPortChanged(const std::string& newPort) {
-    (void)newPort;
+void Marklin6050Interface::serialPortChanged(const std::string& newPort)
+{
+  // For example, reconnect or log
+  (void)newPort;
 }
