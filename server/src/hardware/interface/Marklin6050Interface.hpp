be83deb01710242dc5661ff6cf8d5a45e124baa4
kamil00110 <68923965+kamil00110@users.noreply.github.com>
2025-11-12 02:04:48 +0100

    Update Marklin6050Interface.hpp

--- server/src/hardware/interface/Marklin6050Interface.hpp
+++ server/src/hardware/interface/Marklin6050Interface.hpp
@@ -2,7 +2,7 @@
  * server/src/hardware/interface/Marklin6050Interface.hpp
  *
  * Dummy Märklin 6050 interface for Traintastic
- * © 2025
+ * Copyright (C) 2025
  */
 
 #ifndef TRAINTASTIC_SERVER_HARDWARE_INTERFACE_MARKLIN6050INTERFACE_HPP
@@ -13,7 +13,7 @@
 #include "../../hardware/protocol/Marklin6050Interface/serial.hpp"
 
 /**
- * \brief Cross-platform Märklin 6050 serial interface
+ * \brief Dummy interface for the Märklin 6050 hardware
  */
 class Marklin6050Interface final : public Interface
 {
@@ -28,16 +28,16 @@ private:
   void serialPortChanged(const std::string& newPort);
 
 protected:
-  void addToWorld() override;
-  void loaded() override;
-  void destroying() override;
-  void worldEvent(WorldState state, WorldEvent event) override;
-  bool setOnline(bool& value, bool simulation) override;
+  void addToWorld() final;
+  void loaded() final;
+  void destroying() final;
+  void worldEvent(WorldState state, WorldEvent event) final;
+  void onlineChanged(bool value) final;
+  bool setOnline(bool& value, bool simulation) final;
 
 public:
-  explicit Marklin6050Interface(World& world, std::string_view idValue);
+  Marklin6050Interface(World& world, std::string_view id);
 };
 
 #endif
-
 
