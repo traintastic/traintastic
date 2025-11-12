5eb558191ee57823819879cb1341c994820d8e79
kamil00110 <68923965+kamil00110@users.noreply.github.com>
2025-11-13 00:25:46 +0100

    Update Marklin6050Interface.hpp

--- server/src/hardware/interface/Marklin6050Interface.hpp
+++ server/src/hardware/interface/Marklin6050Interface.hpp
@@ -2,34 +2,32 @@
 #define TRAINTASTIC_SERVER_HARDWARE_INTERFACE_MARKLIN6050INTERFACE_HPP
 
 #include "interface.hpp"
-#include "../../core/objectproperty.hpp"
-#include "../../hardware/protocol/Marklin6050Interface/serial.hpp"
 #include "../../core/serialdeviceproperty.hpp"
+#include "../../core/objectproperty.hpp"
+#include <memory>
 
-
-class Marklin6050Interface : public Interface
+class Marklin6050Interface final : public Interface
 {
   CLASS_ID("interface.marklin6050")
   DEFAULT_ID("marklin6050")
   CREATE_DEF(Marklin6050Interface)
 
 private:
-  SerialDeviceProperty serialPort;
+  std::unique_ptr<class SerialConnection> m_connection;
 
-  void updateEnabled();
-  void serialPortChanged(const std::string& newPort);
-
-protected:
   void addToWorld() final;
   void loaded() final;
   void destroying() final;
-  void worldEvent(WorldState state, WorldEvent event) final;
-  void onlineChanged(bool value);  // remove 'final'
 
+protected:
   bool setOnline(bool& value, bool simulation) final;
 
 public:
-  Marklin6050Interface(World& world, std::string_view id);
+  SerialDeviceProperty device;
+  Property<uint32_t> baudrate;
+
+  Marklin6050Interface(World& world, std::string_view _id);
+  ~Marklin6050Interface() final;
 };
 
 #endif
