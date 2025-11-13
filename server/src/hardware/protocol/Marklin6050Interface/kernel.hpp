da659c4a6436ac959eaaa5a085f0d5a8ef5eb22c
kamil00110 <68923965+kamil00110@users.noreply.github.com>
2025-11-13 16:22:51 +0100

    Update kernel.hpp

--- server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
+++ server/src/hardware/protocol/Marklin6050Interface/kernel.hpp
@@ -1,1 +1,28 @@
+#pragma once
+#include <string>
+
+namespace Marklin6050 {
+
+class Kernel {
+public:
+    Kernel(const std::string& port);
+    ~Kernel();
+
+    bool start();
+    void stop();
+
+    bool sendByte(unsigned char byte);
+
+private:
+    std::string m_port;
+
+#if defined(_WIN32)
+    void* m_handle;
+#else
+    int m_fd;
+#endif
+    bool m_isOpen;
+};
+
+} // namespace Marklin6050
 
