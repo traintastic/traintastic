3ff67df48c1400f873b272cea9c510caec6edb78
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-11-17 11:51:27 +0100

    Update interfaces.cpp

--- server/src/hardware/interface/interfaces.cpp
+++ server/src/hardware/interface/interfaces.cpp
@@ -35,7 +35,6 @@
 #include "wlanmausinterface.hpp"
 #include "xpressnetinterface.hpp"
 #include "z21interface.hpp"
-#include "Marklin6050Interface.hpp"
 
 std::span<const std::string_view> Interfaces::classList()
 {
@@ -49,8 +48,7 @@ std::span<const std::string_view> Interfaces::classList()
     WiThrottleInterface::classId,
     WlanMausInterface::classId,
     XpressNetInterface::classId,
-    Z21Interface::classId,
-    Marklin6050Interface::classId
+    Z21Interface::classId
   );
   return classes;
 }
@@ -67,6 +65,5 @@ std::shared_ptr<Interface> Interfaces::create(World& world, std::string_view cla
   IF_CLASSID_CREATE(WlanMausInterface)
   IF_CLASSID_CREATE(XpressNetInterface)
   IF_CLASSID_CREATE(Z21Interface)
-  IF_CLASSID_CREATE(Marklin6050Interface)
   return std::shared_ptr<Interface>();
 }
