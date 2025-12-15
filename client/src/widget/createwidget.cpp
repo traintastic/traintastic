571b4cb76fc26021ed045bd3b2b492247d25cec9
Kamil Kasprzak <68923965+kamil00110@users.noreply.github.com>
2025-12-15 03:10:56 +0100

    revert to orginal

--- client/src/widget/createwidget.cpp
+++ client/src/widget/createwidget.cpp
@@ -136,52 +136,46 @@ QWidget* createWidget(AbstractProperty& baseProperty, QWidget* parent)
 
 QWidget* createWidget(Property& property, QWidget* parent)
 {
-    QWidget* widget = nullptr;
+  switch(property.type())
+  {
+    case ValueType::Boolean:
+      return new PropertyCheckBox(property, parent);
 
-    switch(property.type())
-    {
-        case ValueType::Boolean:
-            widget = new PropertyCheckBox(property, parent);
-            break;
+    case ValueType::Enum:
+      if(property.enumName() == "pair_output_action")
+      {
+        return new PropertyPairOutputAction(property, parent);
+      }
+      return new PropertyComboBox(property, parent);
 
-        case ValueType::Enum:
-            if(property.enumName() == "pair_output_action")
-                widget = new PropertyPairOutputAction(property, parent);
-            else
-                widget = new PropertyComboBox(property, parent);
-            break;
+    case ValueType::Integer:
+      if(property.hasAttribute(AttributeName::Values) && !property.hasAttribute(AttributeName::Min) && !property.hasAttribute(AttributeName::Max))
+      {
+        return new PropertyComboBox(property, parent);
+      }
+      return new PropertySpinBox(property, parent);
 
-        case ValueType::Integer:
-            if(property.hasAttribute(AttributeName::Values) &&
-               !property.hasAttribute(AttributeName::Min) &&
-               !property.hasAttribute(AttributeName::Max))
-            {
-                widget = new PropertyComboBox(property, parent);
-            }
-            else
-                widget = new PropertySpinBox(property, parent);
-            break;
+    case ValueType::Float:
+      return new PropertyDoubleSpinBox(property, parent);
 
-        case ValueType::Float:
-            widget = new PropertyDoubleSpinBox(property, parent);
-            break;
+    case ValueType::String:
+      if(property.hasAttribute(AttributeName::Values))
+      {
+        return new PropertyComboBox(property, parent);
+      }
+      return new PropertyLineEdit(property, parent);
 
-        case ValueType::String:
-            if(property.hasAttribute(AttributeName::Values))
-                widget = new PropertyComboBox(property, parent);
-            else
-                widget = new PropertyLineEdit(property, parent);
-            break;
+    case ValueType::Object:
+      break; // TODO
 
-        case ValueType::Object: // TODO
-        case ValueType::Set: // TODO
-        case ValueType::Invalid:
-            break;
-    }
+    case ValueType::Set:
+      break; // TODO
 
-    
-
-    return widget;
+    case ValueType::Invalid: /*[[unlikely]]*/
+      break;
+  }
+  assert(false);
+  return nullptr;
 }
 
 QWidget* createWidget(ObjectProperty& property, QWidget* parent)
