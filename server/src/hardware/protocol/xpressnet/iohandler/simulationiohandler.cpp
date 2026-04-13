9481039657d3ebae4c12155d3c760e7a0b06c286
Filippo Gentile <filippogentile@disroot.org>
2026-04-13 23:28:50 +0200

    [xpressnet] Add basic simulation of locomotive drive

--- server/src/hardware/protocol/xpressnet/iohandler/simulationiohandler.cpp
+++ server/src/hardware/protocol/xpressnet/iohandler/simulationiohandler.cpp
@@ -48,16 +48,104 @@ bool SimulationIOHandler::send(const Message& message)
   switch(message.header)
   {
     case STOP_REQUEST:
+    {
       if(message == ResumeOperationsRequest())
         reply(NormalOperationResumed(), 3);
       else if(message == StopOperationsRequest())
         reply(TrackPowerOff(), 3);
       break;
-
+    }
     case SET_STOP_LOCO:
+    {
       if(message == StopAllLocomotivesRequest())
         reply(EmergencyStop(), 3);
       break;
+    }
+    case GET_LOCO_INFO:
+    {
+      const auto& locomotiveInstruction = static_cast<const LocomotiveInstruction&>(message);
+      switch(locomotiveInstruction.identification)
+      {
+        case 0:
+        case idQueryFuncGroup1to3:
+        {
+          Locomotive& loco = m_locomotives[locomotiveInstruction.address()];
+          reply(loco.info);
+          break;
+        }
+        case idQueryFuncGroup4:
+        {
+          Locomotive& loco = m_locomotives[locomotiveInstruction.address()];
+          reply(loco.func13);
+          break;
+        }
+        case idQueryFuncGroup5above:
+        {
+          Locomotive& loco = m_locomotives[locomotiveInstruction.address()];
+          reply(loco.func29);
+          break;
+        }
+      }
+      break;
+    }
+    case SET_LOCO:
+    {
+      const auto& locomotiveInstruction = static_cast<const LocomotiveInstruction&>(message);
+      switch(locomotiveInstruction.identification)
+      {
+        case idSetSpeed14:
+        case idSetSpeed27:
+        case idSetSpeed28:
+        case idSetSpeed128:
+        {
+          speedAndDirectionInstruction(static_cast<const SpeedAndDirectionInstruction&>(locomotiveInstruction));
+          break;
+        }
+        default:
+        {
+          const auto& funcInstruction = static_cast<const FunctionInstructionGroup&>(message);
+          const uint8_t group = funcInstruction.getGroup();
+          if(group == 0)
+            break;
+
+          Locomotive& loco = m_locomotives[funcInstruction.address()];
+          loco.info.setBusy(false);
+          if(group <= 3)
+          {
+            for(int i = 0; i <= 12; i++)
+              loco.info.setFunction(i, funcInstruction.getFunction(i));
+          }
+          else if(group == 4)
+          {
+            for(int i = 13; i <= 28; i++)
+              loco.func13.setFunction(i, funcInstruction.getFunction(i));
+            loco.func13.updateChecksum();
+          }
+          else
+          {
+            for(int i = 29; i <= 68; i++)
+              loco.func29.setFunction(i, funcInstruction.getFunction(i));
+            loco.func29.updateChecksum();
+
+            if(loco.func29.getFunction(68))
+            {
+              // Simulate external change when setting function 68
+              m_kernel.ioContext().post(
+                [this, address=funcInstruction.address()]()
+                {
+                  Locomotive& loco2 = m_locomotives[address];
+                  loco2.info.setBusy(true);
+                  loco2.info.updateChecksum();
+                  m_kernel.receive(LocomotiveBusy(address));
+                });
+            }
+          }
+          loco.info.updateChecksum();
+          break;
+        }
+      }
+      break;
+    }
   }
 
   return true;
@@ -78,6 +166,25 @@ void SimulationIOHandler::reply(const Message& message, const size_t count)
 {
   for(size_t i = 0; i < count; i++)
     reply(message);
+}
+
+void SimulationIOHandler::speedAndDirectionInstruction(const SpeedAndDirectionInstruction& message)
+{
+  Locomotive& loco = m_locomotives[message.address()];
+  loco.info.setBusy(true);
+  loco.info.setDirection(message.direction());
+  loco.info.setSpeedSteps(message.speedSteps());
+  if(message.isEmergencyStop())
+    loco.info.setEmergencyStop();
+  else
+    loco.info.setSpeedStep(message.speedStep());
+
+  if(message.speedSteps() == 14)
+  {
+    const auto& setSpeed14 = static_cast<const SpeedAndDirectionInstruction14&>(message);
+    loco.info.setFunction(0, setSpeed14.getFl());
+  }
+  loco.info.updateChecksum();
 }
 
 }
