a021f95c0fa0b990c83f2b6f69a834206fa77ba3
Reinder Feenstra <reinder@traintastic.org>
2026-07-25 11:21:23 +0200

    [manual] added deprecation warnings for on_x_changed events, the new on_changed must be used

--- manual/luadoc.py
+++ manual/luadoc.py
@@ -667,6 +667,15 @@ class LuaDoc:
                 md += '### `' + item['lua_name'] + '`' + os.linesep + os.linesep
                 md += self._get_term(term_prefix + item['lua_name'].lower() + ':description') + os.linesep + os.linesep
 
+                for qualifier in ['warning', 'note', 'tip']:
+                    description = item_term_prefix + item['lua_name'].lower() + '.' + qualifier + ':description'
+                    if description in self._terms:
+                        md += '!!! ' + qualifier
+                        title = item_term_prefix + item['lua_name'].lower() + '.' + qualifier + ':title'
+                        if title in self._terms:
+                            md += ' "' + self._get_term(title) + '"'
+                        md += os.linesep + textwrap.indent(self._get_term(description), ' ' * 4) + os.linesep + os.linesep
+
                 md += '**Handler signature**' + os.linesep + os.linesep + '`function ('
                 for p in item['parameters']:
                     md += p['name'] + ', '
